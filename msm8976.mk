bc0c3c685f2597e0c5656946daaef1bf3d3a148b
deadman96385 <seanhoyt963@gmail.com>
2017-09-03 08:14:32 -0500

    Remove non-common nfc config

--- msm8976.mk
+++ msm8976.mk
@@ -219,7 +219,6 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-sec-hal.conf:system/etc/libnfc-sec-hal.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:system/etc/libnfc-sec.conf \
     $(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
 
 PRODUCT_PACKAGES += \
