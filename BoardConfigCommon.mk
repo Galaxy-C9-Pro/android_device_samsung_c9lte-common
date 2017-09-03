b7c0463ab09528e4a7bb4280859b77152e5b52e1
deadman96385 <seanhoyt963@gmail.com>
2017-09-03 08:46:17 -0500

    Remove common ril class

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -196,9 +196,6 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
 
-# Telephony
-BOARD_RIL_CLASS := $(COMMON_PATH)/ril
-
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HAS_QCOM_WLAN_SDK := true
