2b0ac0c71e606cba96c4cd86b411e14d93d71b8f
travis mills <travismills82@gmail.com>
2017-09-28 16:45:44 +0800

    add init.target.rc to ramdisk

--- msm8976.mk
+++ msm8976.mk
@@ -209,6 +209,7 @@ PRODUCT_PACKAGES += \
     init.qcom.fm.sh \
     init.qcom.post_boot.sh \
     init.qcom.power.rc \
+    init.target.rc \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
     ueventd.qcom.rc
