8e44f5f19194bb0ba05d35772fd33d59211701a8
Sean Hoyt <seanhoyt963@gmail.com>
2017-10-01 20:23:58 +0800

    Remove releasetools for now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -192,9 +192,6 @@ TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
-# Release tools
-TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
-
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
